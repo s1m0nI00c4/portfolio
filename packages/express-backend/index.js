fb0c8adc5505d3c137d7e117227f0c1078c7e638
SimonLuca Merlante <simonluca.merlante@gmail.com>
2023-07-16 19:10:16 +0200

    did things

--- packages/express-backend/index.js
+++ packages/express-backend/index.js
@@ -1,8 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const URI =
-  "mongodb+srv://simon:simon@cluster0.wmkip.mongodb.net/?retryWrites=true&w=majority";
 const corsOptions = {
   origin: "http://localhost:3000",
 };
@@ -11,7 +10,7 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 mongoose
-  .connect(URI)
+  .connect(process.env.MONGODB_URI)
   .then(() => {
     const PORT = process.env.PORT || 9000;
     app.listen(PORT, () => console.log("Server running on port " + PORT));
